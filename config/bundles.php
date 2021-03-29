db36fd557c9907d410e930d8a3cc547851caeb51
Roro <stefanedr.dev@gmail.com>
2021-03-29 22:47:04 +0200

    EVO - removing api platform

--- config/bundles.php
+++ config/bundles.php
@@ -15,5 +15,4 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
-    ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
 ];
