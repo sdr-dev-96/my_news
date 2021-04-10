1b35491b0c10e595c5cdc3e5a3a24ea83069deb1
Roro <stefanedr.dev@gmail.com>
2021-04-10 16:26:54 +0200

    EVO - gulp minify

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var minify = require('gulp-minify');
 
 gulp.task('sass', function() {
     return gulp.src('source/scss/*.scss')
@@ -7,6 +8,19 @@ gulp.task('sass', function() {
         .pipe(gulp.dest('public/css'));
 });
 
-gulp.task('default', function() {
-    gulp.watch('css/*.scss', gulp.series('sass'));
+gulp.task('min-js', function() {
+    gulp.src('public/js/*.js')
+      .pipe(minify({
+          ext: {
+              min: '.min.js'
+          },
+      }))
+      .pipe(gulp.dest('public/js'))
 });
+
+gulp.task('watch', function() {
+    gulp.watch('public/css/*.scss', gulp.series('sass'));
+    gulp.watch('public/js/*.js', gulp.series('min-js'));
+});
+
+gulp.task('default', gulp.series('watch'));
